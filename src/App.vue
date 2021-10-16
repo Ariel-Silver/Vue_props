6ca391115f471a0b3f76555f6811ee935a43b0d3
Ariel <Ariel.abarca7@gmail.com>
2021-10-16 20:13:10 -0300

    ultimo commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App" class="text-center">
+    <h1 class="py-3">Crear una nueva tarea</h1>
+    <label class="pr-4">Tarea</label>
+    <input class="me-4 ms-4" type="text" v-model="Deberes" placeholder="Ingrese nueva tarea"/>
+    <button class="btn btn-dark btn-lg" @click="Agregar">Agregar</button>
+    <componente
+      :Tareas="Tareas"
+      @eliminardeLista="eliminardeLista"
+    ></componente>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import componente from "./components/componentes.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { componente },
+  data: () => ({
+    Deberes: "",
+    Tareas: ["Limpiar arenero de la gata", "botar la basura"],
+  }),
+  methods:{
+    Agregar: function(){
+      this.Tareas.push(this.Deberes)
+      this.Deberes = "";
+    },
+    eliminardeLista: function (index) {
+      this.Tareas.splice(index, 1);
+      },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+body,html {
+  width: 100%;
+  height: 100%;
+}
+
+p {
+  margin-top: 20px;
+  margin-right: 20px;
+  font-size: large;
+  text-decoration: underline;
+}
+
+button {
+  margin-top: 30px;
+  width: 150px;
+  height: 45px;
+  padding-top: 1px;
+} 
+
+input {
+  width: 500px;
+  border: solid 1px black;
+}
+label {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-weight: 600;
+  font-size: 30px;
+}
+
+ul {
+  font-weight:bold;
+}
+
+li {
+  margin-right: 20px;
 }
 </style>
---
+++ src/components/componentes.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="componente">
+    <ul class="list">
+        <p>To Do:</p>
+      <li v-for="(Tarea, index) of Tareas" :key="index">
+        {{ index }}. {{ Tarea }}
+        <button @click="eliminardeLista(index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "componente",
+  props: {
+    Tareas: {
+      type: Array,
+    },
+  },
+  methods: {
+    eliminardeLista(index) {
+      this.$emit("eliminardeLista", index);
+    },
+  },
+};
+</script>
+
+<style scoped>
+body,html {
+  width: 100%;
+  height: 100%;
+}
+div {
+  background-color: cadetblue;
+  margin: auto;
+  margin-top: 50px;
+  width: 800px;
+  height: 800px;
+  border: solid 3px black;
+}
+p {
+  margin-top: 20px;
+  margin-right: 20px;
+  font-size: large;
+  text-decoration: underline;
+}
+button {
+  margin-top: 30px;
+  width: 150px;
+  height: 45px;
+  padding-top: 1px;
+} 
+input {
+  width: 500px;
+  border: solid 1px black;
+}
+label {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-weight: 600;
+  font-size: 30px;
+}
+ul {
+  font-weight:bold;
+}
+li {
+  margin-right: 20px;
+}
+</style>
